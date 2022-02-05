3f35cef92d82ffe6a9a918171d16b2d9f88ad59b
playck <playck@naver.com>
2022-02-05 22:10:13 +0900

    Add Calendar Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import axios from "axios";
 import React from "react";
 import { RecoilRoot } from "recoil";
+import Calendar from "./components/Calendar";
 
 function App() {
-  return <RecoilRoot>Hello Recoil</RecoilRoot>;
+  return (
+    <RecoilRoot>
+      <Calendar />
+    </RecoilRoot>
+  );
 }
 export default App;
